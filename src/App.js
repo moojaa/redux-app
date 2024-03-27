8d9ffe8119f31ca35627001a81a20f6899a6c4d5
moojaa <129938297+moojaa@users.noreply.github.com>
2024-03-27 17:54:56 +0900

    첫 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import { useDispatch,useSelector } from 'react-redux';
 import './App.css';
+import Box from './component/Box';
 
 function App() {
+  const count = useSelector(state=>state.count)
+  const id = useSelector(state=>state.id)
+  const password = useSelector(state=>state.password)
+  const dispatch = useDispatch()
+  const increase = ()=>{
+    dispatch({type:"INCREMENT",payload:{num:1}})
+  }
+  const increaseF = ()=>{
+    dispatch({type:"INCREMENT",payload:{num:5}})
+  }
+  const login = ()=>{
+    dispatch({type:"LOGIN",payload:{id:"aaa",password:"111"}})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{id}, {password}</h1>
+      <h1>{count}</h1>
+      <Box/>
+      <button onClick={increase}>증가</button>
+      <button onClick={increaseF}>증가5+</button>
+      <button onClick={login}>login</button>
     </div>
   );
 }
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,23 @@
+let initialState = {
+    count: 0,
+    login: "",
+    password: "",
+}
+
+
+function reducer(state = initialState, action) {
+    if (action.type === "INCREMENT") {
+        return { ...state, count: state.count + action.payload.num }
+    }
+    if (action.type === "LOGIN") {
+        return {
+            ...state,
+            id: action.payload.id,
+            password: action.payload.password
+        }
+    }
+
+    return { ...state }
+}
+
+export default reducer
